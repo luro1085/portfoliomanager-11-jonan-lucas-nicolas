6e802d3a49faf8a6fcd3a4591d130bd804c1a8a8
jmanzi <manzi.jonan0409@gmail.com>
2024-08-01 13:42:41 +0000

    extra route on flask skeleton

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/home')
+def index():
+    return "This is the Index Page"
+
 if __name__ == '__main__':
     app.run()
