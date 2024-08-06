0b5060024e70ca794af651adca75cc6717375c0e
Nicolas Ortuno <nicolas.ortuno01@gmail.com>
2024-08-06 14:37:17 +0000

    minor edit in create_tables.py

--- app/database/create_tables.py
+++ app/database/create_tables.py
@@ -29,11 +29,9 @@ CREATE TABLE IF NOT EXISTS transactions (
     purchase_cost DECIMAL(10, 2),
     sale_revenue DECIMAL(10, 2),
     quantity INT,
-    purchase_price DECIMAL(10, 2),
-    sell_price DECIMAL(10, 2),
+    purchase_price_per_share DECIMAL(10, 2),
+    sell_price_per_share DECIMAL(10, 2),
     transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    portfolio_value_before DECIMAL(10, 2) NOT NULL,
-    portfolio_value_after DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (ticker_symbol) REFERENCES stocks(ticker_symbol)
 )
 """)
@@ -46,9 +44,6 @@ CREATE TABLE IF NOT EXISTS assets (
     company_name VARCHAR(100) NOT NULL,
     total_shares_owned INT NOT NULL,
     total_cost DECIMAL(10, 2) NOT NULL,
-    current_total_market_value DECIMAL(10, 2) NOT NULL,
-    total_value_change_from_cost DECIMAL(10, 2) NOT NULL,
-    percentage_value_change_from_cost DECIMAL(10, 2) NOT NULL,
     last_updated_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (ticker_symbol) REFERENCES stocks(ticker_symbol)
 )
