234ea7a2b37eb1faac53e192341b231414485e14
Nicolas Ortuno <nicolas.ortuno01@gmail.com>
2024-08-06 14:35:39 +0000

    got rid of ephemeral values in database

--- app/database/reset_tables.py
+++ app/database/reset_tables.py
@@ -34,11 +34,9 @@ CREATE TABLE IF NOT EXISTS transactions (
     purchase_cost DECIMAL(10, 2),
     sale_revenue DECIMAL(10, 2),
     quantity INT,
-    purchase_price DECIMAL(10, 2),
-    sell_price DECIMAL(10, 2),
+    purchase_price_per_share DECIMAL(10, 2),
+    sell_price_per_share DECIMAL(10, 2),
     transaction_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    portfolio_value_before DECIMAL(10, 2) NOT NULL,
-    portfolio_value_after DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (ticker_symbol) REFERENCES stocks(ticker_symbol)
 )
 """)
@@ -51,9 +49,6 @@ CREATE TABLE IF NOT EXISTS assets (
     company_name VARCHAR(100) NOT NULL,
     total_quantity INT NOT NULL,
     total_cost DECIMAL(10, 2) NOT NULL,
-    current_total_market_value DECIMAL(10, 2) NOT NULL,
-    total_value_change_from_cost DECIMAL(10, 2) NOT NULL,
-    percentage_value_change_from_cost DECIMAL(10, 2) NOT NULL,
     last_updated_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (ticker_symbol) REFERENCES stocks(ticker_symbol)
 )
